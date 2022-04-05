e07bacde1364f3091f7a6d6299ac46a8f206422a
Cagdas Barak <46297862+cagdasbarak@users.noreply.github.com>
2022-04-05 08:08:47 +0300

    Custom Reports

---
+++ Custom Reports/What-Folders-Does-User-Have-Owner?.sql
@@ -0,0 +1,16 @@
+SELECT DISTINCT
+			fp.FolderPath AS [Folder Path]
+			,ufp.[Inherit Permissions]
+		FROM  vUserFolderPermissions ufp WITH (NOLOCK)
+			INNER JOIN vFolderPath fp WITH (NOLOCK)
+				ON fp.FolderId = ufp.FolderId
+			INNER JOIN tbUserGroup ug WITH (NOLOCK)
+				ON ufp.UserId = ug.UserId
+			INNER JOIN tbGroup g WITH (NOLOCK)
+				ON ug.GroupId = g.GroupId
+		WHERE
+			ufp.OrganizationId = #Organization
+			AND ug.UserId = #User
+			AND (g.Active = 1 OR g.IsPersonal = 1)
+			AND ufp.OwnerPermission = 1
+		ORDER BY 1,2
